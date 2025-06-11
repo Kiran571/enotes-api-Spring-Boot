894f7007037403985bb950bdf994f4a24f3c2aca
Kiran571 <kiranvsuryavanshi05@gmail.com>
2025-06-11 18:06:33 +0530

    Day 1- adding classes

---
+++ src/main/java/com/example/entity/Category.java
@@ -0,0 +1,28 @@
+package com.example.entity;
+
+import java.util.Date;
+
+public class Category {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String desc;
+	
+	private Boolean isActive;
+	
+	private Boolean isDeleted;
+	
+	private Integer createdBy;
+	
+	private Date createdDate;
+	
+	private Integer updatedBy;
+	
+	private Date updatedDate;
+	
+	
+	
+	
+}
